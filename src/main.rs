5975c4f737dc738485b948d712e341d3561b1285
soheyHashimoto <heyso1209@gmail.com>
2021-11-13 15:37:53 +0900

    [ADD]clientからbodyを受け取ってスクレイピングする

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{get, web, client, App, HttpServer, Responder, HttpResponse};
+
+mod client;
+
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use client::Client;
 use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
 
@@ -10,26 +14,11 @@ struct Todo {
 
 #[get("/{lyric}/index.html")]
 async fn index(web::Path(lyric): web::Path<String>) -> impl Responder {
-    let body = client::get("https://www.google.com/search?q=%E5%90%9B%E3%81%8C%E5%A5%BD%E3%81%8D+%E6%AD%8C%E8%A9%9E")
-            .finish().unwrap()
-            .send().await.unwrap();
+    let client = Client::new();
 
-    let document = Html::parse_document(&body);
-    let selector = Selector::parse(".kCrYT > a").unwrap();
-
-    for node in document.select(&selector) {
-        let href = node.value().attr("href").unwrap();
-
-        // モジュールであるscraperの場合Googleをスクレイピングすると無駄な文字列が入るので削除
-        let last_index = href.find("&sa=").unwrap();
-        let url = &href[7..last_index];
-
-        if url.contains("uta-net") || url.contains("j-lyric.net") || url.contains("utamap") {
-            println!("{:?}", node.text().collect::<Vec<_>>()[0]);
-            println!("{:?}", url);
-        }
-    }
-    let ele = String::from("Hello, world!");
+    let bytes = client.get().await;
+    let body = &bytes.iter().map(|&s| s as char).collect::<String>();
+    let ele = search_lyric(body).await;
 
     HttpResponse::Ok().json(Todo {
         content: ele,
@@ -37,8 +26,7 @@ async fn index(web::Path(lyric): web::Path<String>) -> impl Responder {
     })
 }
 
-async fn search_lyric() -> String {
-    let body = reqwest::blocking::get("https://www.google.com/search?q=%E5%90%9B%E3%81%8C%E5%A5%BD%E3%81%8D+%E6%AD%8C%E8%A9%9E").unwrap().text().unwrap();
+async fn search_lyric(body: &str) -> String {
     let document = Html::parse_document(&body);
     let selector = Selector::parse(".kCrYT > a").unwrap();
 
